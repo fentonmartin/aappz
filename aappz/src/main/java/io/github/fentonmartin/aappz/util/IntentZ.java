82809f7a121b0128c98d47f480cbd784f12f0cf1
Fenton Martin <fentonxm@gmail.com>
2018-11-04 09:50:27 +0700

    Update IntentZ

--- aappz/src/main/java/io/github/fentonmartin/aappz/util/IntentZ.java
+++ aappz/src/main/java/io/github/fentonmartin/aappz/util/IntentZ.java
@@ -2,15 +2,40 @@ package io.github.fentonmartin.aappz.util;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 
 class IntentZ {
     Intent intent(Context context, Class activity) {
         return new Intent(context, activity);
     }
-
     Intent intent(Context context, Class activity, int flag) {
         Intent intent = new Intent(context, activity);
         intent.setFlags(flag);
+        return intent;
+    }
+    Intent intent(Context context, Class activity, String extra, boolean bool) {
+        Intent intent = new Intent(context, activity);
+        intent.putExtra(extra, bool);
+        return intent;
+    }
+    Intent intent(Context context, Class activity, String extra, int number) {
+        Intent intent = new Intent(context, activity);
+        intent.putExtra(extra, number);
+        return intent;
+    }
+    Intent intent(Context context, Class activity, String extra, String text) {
+        Intent intent = new Intent(context, activity);
+        intent.putExtra(extra, text);
+        return intent;
+    }
+    Intent intent(Context context, Class activity, String extra, Bundle bundle) {
+        Intent intent = new Intent(context, activity);
+        intent.putExtra(extra, bundle);
+        return intent;
+    }
+    Intent intentClear(Context context, Class activity) {
+        Intent intent = new Intent(context, activity);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
         return intent;
     }
 }
