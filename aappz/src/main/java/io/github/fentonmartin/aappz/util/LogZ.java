7bf0bac4a4586d2398bdac3d51718ba3fd88ccd1
Fenton Martin <fentonxm@gmail.com>
2018-11-04 09:02:18 +0700

    Add LogZ Activity

--- aappz/src/main/java/io/github/fentonmartin/aappz/util/LogZ.java
+++ aappz/src/main/java/io/github/fentonmartin/aappz/util/LogZ.java
@@ -1,9 +1,13 @@
 package io.github.fentonmartin.aappz.util;
 
+import android.app.Activity;
 import android.util.Log;
 
 class LogZ {
     void setLog(String log) {
         Log.d("LogZ " + getClass().getSimpleName(), log);
+    }
+    void setLog(Activity activity, String log) {
+        Log.d("LogZ " + activity.getClass().getSimpleName(), log);
     }
 }
