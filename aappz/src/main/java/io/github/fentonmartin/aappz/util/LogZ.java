cbe5cf0b386a0b38f55cb0f91ee95792f269069f
Fenton Martin <fentonxm@gmail.com>
2018-11-04 09:02:47 +0700

    Update setLog

--- aappz/src/main/java/io/github/fentonmartin/aappz/util/LogZ.java
+++ aappz/src/main/java/io/github/fentonmartin/aappz/util/LogZ.java
@@ -5,7 +5,7 @@ import android.util.Log;
 
 class LogZ {
     void setLog(String log) {
-        Log.d("LogZ " + getClass().getSimpleName(), log);
+        Log.d("LogZ ", log);
     }
     void setLog(Activity activity, String log) {
         Log.d("LogZ " + activity.getClass().getSimpleName(), log);
