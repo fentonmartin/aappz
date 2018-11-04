2305713b4f0b732fa8abbdae2381c71434d1d260
Fenton Martin <fentonxm@gmail.com>
2018-11-04 09:37:40 +0700

    Add ToastZ class

---
+++ aappz/src/main/java/io/github/fentonmartin/aappz/util/ToastZ.java
@@ -0,0 +1,13 @@
+package io.github.fentonmartin.aappz.util;
+
+import android.content.Context;
+import android.widget.Toast;
+
+class ToastZ {
+    void setToast(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+    void setToast(Context context, CharSequence message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+}
