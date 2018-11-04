bcd4489f7fd5dbc03155e31443b43a49e2fbf6dc
Fenton Martin <fentonxm@gmail.com>
2018-11-05 00:40:51 +0700

    Update sample main

--- app/src/main/java/io/github/fentonmartin/sample/MainActivity.java
+++ app/src/main/java/io/github/fentonmartin/sample/MainActivity.java
@@ -1,12 +1,17 @@
 package io.github.fentonmartin.sample;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import io.github.fentonmartin.aappz.AappZ;
+import io.github.fentonmartin.aappz.constant.DateConstant;
+import io.github.fentonmartin.aappz.util.DateZ;
 
 public class MainActivity extends AappZ {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +19,21 @@ public class MainActivity extends AappZ {
 
         Button button = findViewById(R.id.button);
         setViewEnabled(button, false);
+
+        TextView textView = findViewById(R.id.text);
+        textView.setText(DateZ.getDatetime() + "\n" +
+                DateZ.getDatetime(DateConstant.DATE_FULL_REVERSE) + "\n" +
+                DateZ.getDatetime(DateConstant.DATE_DAY) + "\n" +
+                DateZ.getDatetime(DateConstant.DATE_DAY_REVERSE) + "\n" +
+                DateZ.getDatetime(DateConstant.DATE_DAY_FULL) + "\n" +
+                DateZ.getDatetime(DateConstant.DATE_DAY_FULL_REVERSE) + "\n" +
+                DateZ.getDatetime(DateConstant.TIME) + "\n" +
+                DateZ.getDatetime(DateConstant.TIME_FULL) + "\n" +
+                DateZ.getDatetime(DateConstant.TIME_12) + "\n" +
+                DateZ.getDatetime(DateConstant.TIME_12_FULL) + "\n" +
+                DateZ.getTimestampString() + "\n" +
+                DateZ.getTimestampTime(910198934000L) + "\n" +
+                DateZ.getTimestampTime("910198934000") + "\n" +
+                DateZ.getDatetimeSimple());
     }
 }
