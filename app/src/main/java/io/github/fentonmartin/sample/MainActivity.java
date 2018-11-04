57878710b26260cb3b8a2b188dde96bc49ad6831
Fenton Martin <fentonxm@gmail.com>
2018-11-04 23:11:59 +0700

    Update sample main

--- app/src/main/java/io/github/fentonmartin/sample/MainActivity.java
+++ app/src/main/java/io/github/fentonmartin/sample/MainActivity.java
@@ -1,6 +1,7 @@
 package io.github.fentonmartin.sample;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import io.github.fentonmartin.aappz.AappZ;
 
@@ -10,5 +11,8 @@ public class MainActivity extends AappZ {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        setViewEnabled(button, false);
     }
 }
