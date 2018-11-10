ce04ff5f42355deaa3d35dcea1d9ef975d2b40d8
Fenton Martin <fentonxm@gmail.com>
2018-11-11 01:38:19 +0700

    Add PrefZ Initialization on AappZ

--- aappz/src/main/java/io/github/fentonmartin/aappz/AappZ.java
+++ aappz/src/main/java/io/github/fentonmartin/aappz/AappZ.java
@@ -1,7 +1,23 @@
 package io.github.fentonmartin.aappz;
 
+import android.content.ContextWrapper;
+import android.os.Bundle;
+
 import io.github.fentonmartin.aappz.util.ActivityZ;
+import io.github.fentonmartin.aappz.util.PrefZ;
 
 public class AappZ extends ActivityZ {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        /* PrefZ Initialization */
+        new PrefZ.Builder()
+                .setContext(this)
+                .setMode(ContextWrapper.MODE_PRIVATE)
+                .setPrefsName(getPackageName())
+                .setUseDefaultSharedPreference(true)
+                .build();
+    }
 }
