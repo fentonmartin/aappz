eb39e4f9a11ce8f15f9e34f08d19ce4b7c1e98ae
Fenton Martin <fentonxm@gmail.com>
2018-11-10 19:08:09 +0700

    Update setLog with Debug

--- aappz/src/main/java/io/github/fentonmartin/aappz/util/LogZ.java
+++ aappz/src/main/java/io/github/fentonmartin/aappz/util/LogZ.java
@@ -7,7 +7,18 @@ class LogZ {
     void setLog(String log) {
         Log.d("LogZ ", log);
     }
+
     void setLog(Activity activity, String log) {
         Log.d("LogZ " + activity.getClass().getSimpleName(), log);
+    }
+
+    void setLog(String log, boolean isDebug) {
+        if (isDebug)
+            Log.d("LogZ ", log);
+    }
+
+    void setLog(Activity activity, String log, boolean isDebug) {
+        if (isDebug)
+            Log.d("LogZ " + activity.getClass().getSimpleName(), log);
     }
 }
