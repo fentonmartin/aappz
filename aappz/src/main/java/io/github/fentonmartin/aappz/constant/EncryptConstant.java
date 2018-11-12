5f8533b2caefa5baa1f7cf04ff2fe99f2d19fcbb
Fenton Martin <fentonxm@gmail.com>
2018-11-12 16:12:04 +0700

    Add EncryptConstant

---
+++ aappz/src/main/java/io/github/fentonmartin/aappz/constant/EncryptConstant.java
@@ -0,0 +1,17 @@
+package io.github.fentonmartin.aappz.constant;
+
+public class EncryptConstant {
+    private static String NUMERIC_ASC = "0123456789";
+    private static String NUMERIC_DSC = "9876543210";
+    private static String ALPHABET_LOWER_ASC = "abcdefghijklmnopqrstuvwxyz";
+    private static String ALPHABET_LOWER_DSC = "zyxwvutsrqponmlkjihgfedcba";
+    private static String ALPHABET_UPPER_ASC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static String ALPHABET_UPPER_DSC = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
+
+    public static char[] ARRAY_NUMERIC_ASC = NUMERIC_ASC.toCharArray();
+    public static char[] ARRAY_NUMERIC_DSC = NUMERIC_DSC.toCharArray();
+    public static char[] ARRAY_ALPHABET_LOWER_ASC = ALPHABET_LOWER_ASC.toCharArray();
+    public static char[] ARRAY_ALPHABET_LOWER_DSC = ALPHABET_LOWER_DSC.toCharArray();
+    public static char[] ARRAY_ALPHABET_UPPER_ASC = ALPHABET_UPPER_ASC.toCharArray();
+    public static char[] ARRAY_ALPHABET_UPPER_DSC = ALPHABET_UPPER_DSC.toCharArray();
+}
