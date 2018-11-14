d86882e9b603a2b3b596a3ac085e2d2952870d48
Fenton Martin <fentonxm@gmail.com>
2018-11-14 17:03:40 +0700

    Add EncryptConstant

--- aappz/src/main/java/io/github/fentonmartin/aappz/constant/EncryptConstant.java
+++ aappz/src/main/java/io/github/fentonmartin/aappz/constant/EncryptConstant.java
@@ -16,4 +16,9 @@ public class EncryptConstant {
     public static char[] ARRAY_ALPHABET_LOWER_DSC = ALPHABET_LOWER_DSC.toCharArray();
     public static char[] ARRAY_ALPHABET_UPPER_ASC = ALPHABET_UPPER_ASC.toCharArray();
     public static char[] ARRAY_ALPHABET_UPPER_DSC = ALPHABET_UPPER_DSC.toCharArray();
+
+    public static String ENCRYPT_62 = "5KkR0ZHweNfjCLUp1gA2sSYx4MGhVnOtJI8yWPouXEcb7zmDQlir36aFdBTv9q";
+    public static String ENCRYPT_124 = "7YtUbj9ZJzS1DeLbRMqeejFr0PUXzL2uhSAFpXwaf5UJm2f8Dzxn2XF3OU7yIfxdwixJYtLvO7HSRnvuxvq65H3Znq7XPU1HXyrlQfYmH5Vw1m9XpkmQcYmGjAjp";
+    public static String ENCRYPT_186 = "1y2U07RBjnWQfwv73g3Hfeq00v1I8SbScbsg1mJbMkswnoRPtTZ6DvCPwyzgE2XsAMiEUmQOm9cg8YsOvGhuIoRRUI1OXL8ZYMhepl8J2H6AxCVRxdbWRgHPaCBcTDA9WjW9l7dsEex5Mpbt1N7xb2tQXi7KRCJ2QvaCKjd0gxcb1N7cUztVub2mW2";
+    public static String ENCRYPT_248 = "RjRCziawLCcfkRQHFTV0av91E5iYI3KtL5zlfjY30bsDreq6y0QvxjDjjkXFjkPWOzh5IGUyZlLi9kQmo9o5agZZ1sbGZNLAXXu17vnHq0rk7s7bAzWmIELysQR1Bbwl6NW5LgeR98aCrFhoRxEZIWqachV1PXhyTXsokldxqmUdINQ4HlVd8JXS1DjJlg6BIFAWTkDxKKenS2Oi64gBsbdBLq6L6yMaY3NT5oBHZ3SGMfS1pUf34zYy";
 }
