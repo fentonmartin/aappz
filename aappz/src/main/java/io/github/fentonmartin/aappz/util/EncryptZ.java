59be9bd7a91136aebc169c34f15805b2994bf775
Fenton Martin <fentonxm@gmail.com>
2018-12-15 21:31:48 +0700

    Update EncryptZ Functions

--- aappz/src/main/java/io/github/fentonmartin/aappz/util/EncryptZ.java
+++ aappz/src/main/java/io/github/fentonmartin/aappz/util/EncryptZ.java
@@ -9,15 +9,6 @@ public class EncryptZ {
 
     private static Map<String, String> map;
 
-    /**
-     * Preparation for make default of Map(String, String)
-     *
-     * @param input       is a collection of input characters
-     * @param inputIndex  is an index of input characters
-     * @param output      is a collection of output characters
-     * @param outputIndex is an index of output characters
-     * @return Map(String, String) from key
-     */
     private static Map<String, String> prepare(String input, String output, int inputIndex, int outputIndex) {
         map = new HashMap<>();
         String[] inputs = TextZ.convertStringToArray(input, inputIndex);
@@ -28,39 +19,18 @@ public class EncryptZ {
         return map;
     }
 
-    /**
-     * Preparation for make encryption based on Map(String, String)
-     *
-     * @param key   is a collection of characters
-     * @param index is an index of characters
-     * @return Map(String, String) from key
-     */
     private static Map<String, String> prepareEncrypt(String key, int index) {
         return prepare(EncryptConstant.ALL, key, 1, index);
     }
 
-    /**
-     * Preparation for make decryption based on Map(String, String)
-     *
-     * @param key   is a collection of characters
-     * @param index is an index of characters
-     * @return Map(String, String) from key
-     */
     private static Map<String, String> prepareDecrypt(String key, int index) {
         return prepare(key, EncryptConstant.ALL, index, 1);
     }
 
-    /**
-     * Encryption based on Map(String, String) key
-     *
-     * @param text is an unencrypted text/string/sentences
-     * @param key  is a collection of characters
-     * @return ENCRYPTED Map(String, String) from key
-     */
-    public static String encryptTo(String text, String key) {
-        if (key.length() != 86)
+    private static String encryptTo(String text, String key, int index) {
+        if (key.length() != 86 * index)
             return "";
-        map = prepareEncrypt(key, 1);
+        map = prepareEncrypt(key, index);
         StringBuilder temp = new StringBuilder();
         for (int i = 0; i < text.length(); i++) {
             temp.append(map.get(String.valueOf(text.substring(i, i + 1))));
@@ -68,21 +38,38 @@ public class EncryptZ {
         return temp.toString();
     }
 
+    private static String decryptTo(String text, String key, int index) {
+        map = prepareDecrypt(key, index);
+        StringBuilder temp = new StringBuilder();
+        for (int i = 0; i < text.length() / index; i++) {
+            temp.append(map.get(String.valueOf(text.substring(i * index, (i * index) + index))));
+        }
+        return temp.toString();
+    }
+
+    /**
+     * Encryption based on Map(String, String) key
+     *
+     * @param text is an unencrypted text/string/sentences
+     * @param key  is a collection of multiples of 86 characters
+     * @return ENCRYPTED Map(String, String) from key
+     */
+    public static String encryptTo(String text, String key) {
+        if (key.length() % 86 > 0)
+            return "";
+        return encryptTo(text, key, key.length() / 86);
+    }
+
     /**
      * Decryption based on Map(String, String) key
      *
      * @param text is an encrypted text/string/sentences
-     * @param key  is a collection of characters
+     * @param key  is a collection of multiples of 86 characters
      * @return DECRYPTED Map(String, String) from key
      */
     public static String decryptTo(String text, String key) {
-        if (key.length() != 86)
+        if (key.length() % 86 > 0)
             return "";
-        map = prepareDecrypt(key, 1);
-        StringBuilder temp = new StringBuilder();
-        for (Character c : text.toCharArray()) {
-            temp.append(map.get(String.valueOf(c)));
-        }
-        return temp.toString();
+        return decryptTo(text, key, key.length() / 86);
     }
 }
