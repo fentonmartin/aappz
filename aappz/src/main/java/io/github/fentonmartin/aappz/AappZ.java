3057d076c70c212ec0d3c87399f517a7fb813002
fentonmartin <fentonxm@gmail.com>
2019-01-15 13:33:40 +0700

    PermissionZ Initialization on AappZ

--- aappz/src/main/java/io/github/fentonmartin/aappz/AappZ.java
+++ aappz/src/main/java/io/github/fentonmartin/aappz/AappZ.java
@@ -2,11 +2,18 @@ package io.github.fentonmartin.aappz;
 
 import android.content.ContextWrapper;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 
+import java.util.List;
+
+import io.github.fentonmartin.aappz.implementation.OnPermissionListener;
 import io.github.fentonmartin.aappz.util.ActivityZ;
+import io.github.fentonmartin.aappz.util.PermissionZ;
 import io.github.fentonmartin.aappz.util.PrefZ;
 
 public class AappZ extends ActivityZ {
+
+    public PermissionZ permissionZ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +26,33 @@ public class AappZ extends ActivityZ {
                 .setPrefsName(getPackageName())
                 .setUseDefaultSharedPreference(true)
                 .build();
+
+        /* PermissionZ Initialization */
+        permissionZ = new PermissionZ
+                .Builder()
+                .with(this)
+                .listener(new OnPermissionListener() {
+                    @Override
+                    public void onAllPermissionsGranted(@NonNull List<String> permissions) {
+                        setLog("PermissionZ: All permissions granted");
+                    }
+
+                    @Override
+                    public void onPermissionsGranted(@NonNull List<String> permissions) {
+                        setLog("PermissionZ: Permissions is granted");
+                    }
+
+                    @Override
+                    public void onPermissionsDenied(@NonNull List<String> permissions) {
+                        setLog("PermissionZ: Permissions is denied");
+                    }
+                })
+                .build();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        this.permissionZ.onRequestPermissionsResult(permissions, grantResults);
     }
 }
