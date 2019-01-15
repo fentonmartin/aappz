660aa9273cf392cbb96397fe5277b9965480c4ab
fentonmartin <fentonxm@gmail.com>
2019-01-15 13:00:26 +0700

    Add PermissionZ and OnPermissionListener

---
+++ aappz/src/main/java/io/github/fentonmartin/aappz/implementation/OnPermissionListener.java
@@ -0,0 +1,14 @@
+package io.github.fentonmartin.aappz.implementation;
+
+import android.support.annotation.NonNull;
+
+import java.util.List;
+
+public interface OnPermissionListener {
+
+    void onAllPermissionsGranted(@NonNull List<String> permissions);
+
+    void onPermissionsGranted(@NonNull List<String> permissions);
+
+    void onPermissionsDenied(@NonNull List<String> permissions);
+}
