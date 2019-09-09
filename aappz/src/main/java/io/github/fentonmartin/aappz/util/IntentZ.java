a5eeb72c4fca11bf2165e3d10d6d07ab5b256e9c
Fenton Martin <fentonxm@gmail.com>
2019-09-09 15:02:02 +0700

    Update IntentZ

--- aappz/src/main/java/io/github/fentonmartin/aappz/util/IntentZ.java
+++ aappz/src/main/java/io/github/fentonmartin/aappz/util/IntentZ.java
@@ -6,45 +6,48 @@ import android.net.Uri;
 import android.os.Bundle;
 
 class IntentZ {
+
     Intent intent(Context context, Class activity) {
         return new Intent(context, activity);
     }
-    Intent intent(Context context, Class activity, int flag) {
-        Intent intent = new Intent(context, activity);
-        intent.setFlags(flag);
-        return intent;
-    }
+
     Intent intent(Context context, Class activity, String extra, boolean bool) {
         Intent intent = new Intent(context, activity);
         intent.putExtra(extra, bool);
         return intent;
     }
-    Intent intent(Context context, Class activity, String extra, int number) {
-        Intent intent = new Intent(context, activity);
-        intent.putExtra(extra, number);
-        return intent;
-    }
+
     Intent intent(Context context, Class activity, String extra, String text) {
         Intent intent = new Intent(context, activity);
         intent.putExtra(extra, text);
         return intent;
     }
+
     Intent intent(Context context, Class activity, String extra, Bundle bundle) {
         Intent intent = new Intent(context, activity);
         intent.putExtra(extra, bundle);
         return intent;
     }
+
+    Intent intent(Context context, Class activity, int flag) {
+        Intent intent = new Intent(context, activity);
+        intent.setFlags(flag);
+        return intent;
+    }
+
     Intent intentClear(Context context, Class activity) {
         Intent intent = new Intent(context, activity);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
         return intent;
     }
+
     Intent intentEmail(String email, String subject) {
         Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", email, null));
         intent.putExtra(Intent.EXTRA_SUBJECT, subject);
         intent.putExtra(Intent.EXTRA_TEXT, "");
         return intent;
     }
+
     Intent intentMarket(String id) {
         Intent intent = new Intent(Intent.ACTION_VIEW);
         try {
@@ -54,6 +57,7 @@ class IntentZ {
         }
         return intent;
     }
+
     Intent intentSend(String subject, String text) {
         Intent intent = new Intent(android.content.Intent.ACTION_SEND);
         intent.setType("text/plain");
@@ -61,6 +65,7 @@ class IntentZ {
         intent.putExtra(android.content.Intent.EXTRA_TEXT, text);
         return intent;
     }
+
     Intent intentWebsite(String website) {
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setData(Uri.parse(website));
