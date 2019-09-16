ec2c444d4b5de14aca5193744e5b4723911ba27b
Fenton Martin <fentonxm@gmail.com>
2019-09-16 20:18:05 +0700

    Add DelayZ

---
+++ aappz/src/main/java/io/github/fentonmartin/aappz/util/DelayZ.java
@@ -0,0 +1,33 @@
+package io.github.fentonmartin.aappz.util;
+
+import android.os.Handler;
+
+public class DelayZ {
+
+    private static Handler handler = new Handler();
+
+    public static void post(long millis, final Callback callback) {
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                callback.onDelayed();
+            }
+        }, millis);
+    }
+
+    public static void post(long millis, Runnable runnable) {
+        handler.postDelayed(runnable, millis);
+    }
+
+    public static void cancel(Runnable runnable) {
+        handler.removeCallbacksAndMessages(runnable);
+    }
+
+    public static void cancel() {
+        handler.removeCallbacksAndMessages(null);
+    }
+
+    public interface Callback {
+        void onDelayed();
+    }
+}
