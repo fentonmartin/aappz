2c2185d5233af87e89348601bd6583a88823e483
Fenton Martin <fentonxm@gmail.com>
2019-11-04 16:22:42 +0700

    Add Bounce Animation

---
+++ aappz/src/main/java/io/github/fentonmartin/aappz/anim/BounceAnimation.java
@@ -0,0 +1,17 @@
+package io.github.fentonmartin.aappz.anim;
+
+public class BounceAnimation implements android.view.animation.Interpolator {
+
+    private double mAmplitude;
+    private double mFrequency;
+
+    public BounceAnimation(double amplitude, double frequency) {
+        mAmplitude = amplitude;
+        mFrequency = frequency;
+    }
+
+    public float getInterpolation(float time) {
+        return (float) (-1 * Math.pow(Math.E, -time / mAmplitude) *
+                Math.cos(mFrequency * time) + 1);
+    }
+}
