089e26c4530521c393833aa0d1bb15abab117bac
Fenton Martin <fentonxm@gmail.com>
2019-11-09 22:03:32 +0700

    Update DeviceZ

--- aappz/src/main/java/io/github/fentonmartin/aappz/util/DeviceZ.java
+++ aappz/src/main/java/io/github/fentonmartin/aappz/util/DeviceZ.java
@@ -20,6 +20,36 @@ import static android.content.Context.TELEPHONY_SERVICE;
 @SuppressWarnings("unused")
 public class DeviceZ {
 
+/*----------------------------------------------------------------------
+    Property                Examples
+------------------------------------------------------------------------
+    Build.BOARD             MSM8974
+    Build.BOOTLOADER        s1
+    Build.BRAND             Sony
+    Build.CPU_ABI           armeabi-v7a
+    Build.CPU_ABI2          armeabi
+    Build.DEVICE            D5503
+    Build.DISPLAY           14.6.A.1.236
+    Build.FINGERPRINT       Sony/D5503/D5503:5.1.1/14.6.A.1.236/2031203XXX:user/release-keys
+    Build.HARDWARE          qcom
+    Build.HOST              BuildHost
+    Build.ID                14.6.A.1.236
+    Build.IS_DEBUGGABLE     false
+    Build.MANUFACTURER      Sony
+    Build.MODEL             D5503
+    Build.PRODUCT           D5503
+    Build.RADIO             unknown
+    Build.SERIAL            CB5A1YGVMT
+    Build.SUPPORTED_32_BIT_ABIS     [Ljava.lang.String;@3dd90541
+    Build.SUPPORTED_64_BIT_ABIS     [Ljava.lang.String;@1da4fc3
+    Build.SUPPORTED_ABIS            [Ljava.lang.String;@525f635
+    Build.TAGS              release-keys
+    Build.TIME              144792559XXXX
+    Build.TYPE              user
+    Build.UNKNOWN           unknown
+    Build.USER              BuildUser
+------------------------------------------------------------------------ */
+
     /**
      * Get Android ID from device
      * <p>
@@ -41,6 +71,7 @@ public class DeviceZ {
      * Identify a particular installation (not a physical device). Method generates an unique
      * identifier for a specific installation. You have just to store that value and your
      * user will be identified at the next launch of your application.
+     *
      * @return the result
      */
     public static String getUUID() {
@@ -96,6 +127,7 @@ public class DeviceZ {
      * It read details like ROM Version, Manufacturer name, CPU type, and other hardware details.
      * This is possible get two devices with the same ID, based on the same hardware and rom image.
      * No special permission are required, making this approach very convenient.
+     *
      * @return the result
      */
     public static String getPseudoUniqueID() {
@@ -107,5 +139,9 @@ public class DeviceZ {
                 Build.MODEL.length() % 10 + Build.PRODUCT.length() % 10 +
                 Build.TAGS.length() % 10 + Build.TYPE.length() % 10 +
                 Build.USER.length() % 10;
+    }
+
+    public static String getPhoneModel() {
+        return TextZ.getFormatName(Build.MANUFACTURER) + " " + Build.MODEL;
     }
 }
