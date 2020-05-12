ba9b38b4b9aca33d201b48830054c157448fb398
Fenton Martin <fentonxm@gmail.com>
2020-05-12 21:41:51 +0700

    Add DateActivity

---
+++ app/src/main/java/io/github/fentonmartin/sample/DateActivity.java
@@ -0,0 +1,20 @@
+package io.github.fentonmartin.sample;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import io.github.fentonmartin.aappz.AappZ;
+
+public class DateActivity extends AappZ {
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
