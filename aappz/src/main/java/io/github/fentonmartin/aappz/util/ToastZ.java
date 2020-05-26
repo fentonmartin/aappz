8b88dc5bb8e0cff3e018bf1c694036b86a37def1
Fenton Martin <fentonxm@gmail.com>
2020-05-26 20:38:04 +0700

    Update ToastZ

--- aappz/src/main/java/io/github/fentonmartin/aappz/util/ToastZ.java
+++ aappz/src/main/java/io/github/fentonmartin/aappz/util/ToastZ.java
@@ -4,9 +4,11 @@ import android.content.Context;
 import android.widget.Toast;
 
 class ToastZ {
+
     void setToast(Context context, String message) {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
     }
+
     void setToast(Context context, CharSequence message) {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
     }
