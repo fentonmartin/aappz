fc8a39890e9786f1e19aa0b54720e0cbbba0c277
Fenton Martin <fentonxm@gmail.com>
2020-05-31 06:58:04 +0700

    Add location utils

---
+++ aappz/src/main/java/io/github/fentonmartin/aappz/location/util/LocationAccuracy.java
@@ -0,0 +1,8 @@
+package io.github.fentonmartin.aappz.location.util;
+
+public enum LocationAccuracy {
+    LOWEST,
+    LOW,
+    MEDIUM,
+    HIGH
+}
---
+++ aappz/src/main/java/io/github/fentonmartin/aappz/location/util/TransitionGeofence.java
@@ -0,0 +1,23 @@
+package io.github.fentonmartin.aappz.location.util;
+
+/**
+ * Wraps Geofences and Transitions
+ */
+public class TransitionGeofence {
+
+    private GeofenceModel geofenceModel;
+    private int transitionType;
+
+    public TransitionGeofence(GeofenceModel geofence, int transitionType) {
+        this.geofenceModel = geofence;
+        this.transitionType = transitionType;
+    }
+
+    public GeofenceModel getGeofenceModel() {
+        return geofenceModel;
+    }
+
+    public int getTransitionType() {
+        return transitionType;
+    }
+}
