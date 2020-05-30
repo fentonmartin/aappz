7edd6bcc365033974ea69ce838e34fdf6955b57f
Fenton Martin <fentonxm@gmail.com>
2020-05-31 06:43:50 +0700

    Add ActivityListener

---
+++ aappz/src/main/java/io/github/fentonmartin/aappz/location/listener/ActivityListener.java
@@ -0,0 +1,19 @@
+package io.github.fentonmartin.aappz.location.listener;
+
+import android.content.Context;
+
+import com.google.android.gms.location.DetectedActivity;
+
+import io.github.fentonmartin.aappz.location.util.ActivityParams;
+import io.github.fentonmartin.aappz.util.LogZ;
+
+public interface ActivityListener {
+
+    void init(Context context, LogZ.Logger logger);
+
+    void start(OnActivityUpdatedListener listener, ActivityParams params);
+
+    void stop();
+
+    DetectedActivity getLastActivity();
+}
