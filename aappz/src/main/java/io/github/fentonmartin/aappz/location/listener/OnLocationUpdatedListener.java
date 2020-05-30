c3cd988c091142ddf727c551f4882d7bc68649c5
Fenton Martin <fentonxm@gmail.com>
2020-05-31 06:47:20 +0700

    Add OnLocationUpdatedListener

---
+++ aappz/src/main/java/io/github/fentonmartin/aappz/location/listener/OnLocationUpdatedListener.java
@@ -0,0 +1,9 @@
+package io.github.fentonmartin.aappz.location.listener;
+
+import android.location.Location;
+
+public interface OnLocationUpdatedListener {
+
+    void onLocationUpdated(Location location);
+
+}
