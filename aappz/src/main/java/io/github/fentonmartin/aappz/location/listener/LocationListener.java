bd5163beb7ff8bfeb1ae2c522b14661f86e86074
Fenton Martin <fentonxm@gmail.com>
2020-05-31 06:46:02 +0700

    Add LocationListener

---
+++ aappz/src/main/java/io/github/fentonmartin/aappz/location/listener/LocationListener.java
@@ -0,0 +1,19 @@
+package io.github.fentonmartin.aappz.location.listener;
+
+import android.content.Context;
+import android.location.Location;
+
+import io.github.fentonmartin.aappz.location.util.LocationParams;
+import io.github.fentonmartin.aappz.util.LogZ;
+
+public interface LocationListener {
+
+    void init(Context context, LogZ.Logger logger);
+
+    void start(OnLocationUpdatedListener listener, LocationParams params, boolean singleUpdate);
+
+    void stop();
+
+    Location getLastLocation();
+
+}
