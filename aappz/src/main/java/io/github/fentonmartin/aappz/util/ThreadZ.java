761904d5be3a9f1ffa12bd59abef7512eca45ae8
Fenton Martin <fentonxm@gmail.com>
2020-08-17 18:14:53 +0700

    Update ThreadZ

--- aappz/src/main/java/io/github/fentonmartin/aappz/util/ThreadZ.java
+++ aappz/src/main/java/io/github/fentonmartin/aappz/util/ThreadZ.java
@@ -2,6 +2,12 @@ package io.github.fentonmartin.aappz.util;
 
 public class ThreadZ {
 
+    public static void request(final ThreadZ.Callback callback) {
+        if (PrefZ.getLong("ThreadZ") <= 0)
+            PrefZ.setLong("ThreadZ", DateZ.getTimestamp());
+        valid(PrefZ.getLong("ThreadZ"), callback);
+    }
+
     public static void valid(long timestamp, final ThreadZ.Callback callback) {
         if (DateZ.getTimeRangeCheck(timestamp, DateZ.MINUTES))
             callback.onValid();
